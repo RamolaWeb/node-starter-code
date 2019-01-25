86497da8c8d5f38e8968b0b0f41d83ce91d54d90
sahil.ebizon <sahil.ebizon@ebizontek.com>
2019-01-25 20:39:00 +0530

    Added Basic Util for the JWT Auth

---
+++ utils/auth.js
@@ -0,0 +1,43 @@
+import jwt from 'jsonwebtoken'
+
+const privateKey = process.env.PRIVATE_KEY || 'private'
+const publicKey = process.env.PUBLIC_KEY || 'public'
+
+/** This function generate the token with RSA256 Algorithm
+ * @param {string} time The Time at which the token expire
+ * @param {Object} payload This is the id of the user
+ * @param {Object} signingOption This is the Siging Option of the Server
+ * Issuer is the Email of the Signing Authority
+ * Subject is the email of the reciept user
+ */
+export const generateToken = (payload, { issuer, subject }, time = '30d') => {
+  const signingOption = {
+    issuer,
+    subject,
+    expiresIn: time,
+    algorithm: 'RSA256',
+  }
+  return jwt.sign(payload, privateKey, signingOption)
+}
+
+/** This function verify whether the recieved token is verified or not
+ * @param {String} token The token which is recived by the server
+ * @param {Object} signingOption This is the siging option of the server
+ * @param {String} time The time at which token expire
+ * Issuer is the Email of the Signing Authority
+ * Subject is the email of the reciept user
+ */
+export const verifyToken = (token, { issuer, subject }, time = '30d') => {
+  const verifyingOption = {
+    issuer,
+    subject,
+    expiresIn: time,
+    algorithm: ['RSA256'],
+  }
+  try {
+    return jwt.verify(token, publicKey, verifyingOption)
+  }
+  catch (err) {
+    throw err
+  }
+}
